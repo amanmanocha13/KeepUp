5791de477795e802a6643fbd49c28880da57dfc3
Aman Manocha <aman.manocha1308@gmail.com>
2020-06-23 13:30:42 +0530

    change connection string

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 //connecting to mongodb with db name as KeepUp
+let connectionString = `mongodb+srv://keepup:${env.db_password}@cluster0-w2xfu.mongodb.net/${env.db_name}?retryWrites=true&w=majority`;
 mongoose.connect(`mongodb://localhost/K${env.db_name}`);
 
 const db = mongoose.connection;
